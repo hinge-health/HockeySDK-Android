c319d9e4dc5760e6068be8f36689e4e42e35606c
Fabien Demangeat <fabien@hingehealth.com>
2019-09-05 15:42:23 +0100

    Remove metrics folder

--- hockeysdk/src/main/java/net/hockeyapp/android/metrics/model/TelemetryData.java
+++
@@ -1,8 +0,0 @@
-package net.hockeyapp.android.metrics.model;
-
-import net.hockeyapp.android.metrics.ITelemetry;
-
-import java.io.Serializable;
-
-public abstract class TelemetryData extends ITelemetry implements Serializable {
-}
